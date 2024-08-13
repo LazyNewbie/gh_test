007ab406d6d27a2783d13a0e9e3738671a217f48
AlexeyAtroshenko <alexey@exads.com>
2024-08-13 12:22:31 +0100

    f

---
+++ src/Exads/Repos/Repo2.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Exads\Repos;
+
+class Repo2
+{
+
+    public function getString(): string
+    {
+        return "string 1";
+    }
+
+    public function getString2(): string
+    {
+        return "string 2";
+    }
+}
